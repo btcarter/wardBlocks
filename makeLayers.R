ae7a870e540c98ef15196996a7d67e8134612328
btcarter <benjamin.t.carter@gmail.com>
2019-08-28 12:28:14 -0600

    got the polygon to construct finally, now just to add the dataframe

--- makeLayers.R
+++ makeLayers.R
@@ -37,41 +37,25 @@ writeRaster(AERIAL,paste(OUTPUT,"satImage.tif",sep="/"),format="GTiff",overwrite
 dfA <- read.delim(paste(OUTPUT,"block_A.txt",sep="/"),header = TRUE)
 dfB <- read.delim(paste(OUTPUT,"block_B.txt",sep="/"),header = TRUE)
 
-# make individual block polygons
-makePoly <- function(coordinateSource) {
-  NAME <- gsub("~\\/Box\\/alu\\/spatialData\\/block_(\\w).txt","\\1",coordinateSource)
-  p <- read.delim(coordinateSource,header = TRUE)
-  p <- Polygon(p,hole=FALSE)
-  p <- Polygons(list(p),1)
-  p <- SpatialPolygons(list(p))
-  proj4string(p) <- CRS("+proj=utm +zone=12 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
-  p <- SpatialPolygonsDataFrame(p,data.frame(block=NAME))
-  return(assign(paste("block_",NAME,sep=""),p))
-}
-
+# make ward block polygons
 BLOCKS <- c(paste(OUTPUT,"/block_",LETTERS[1:12],".txt",sep=""))
-WARD <- lapply(BLOCKS,makePoly) # make the polygons and store in a list called WARD
 
-i <- 1
-while (i < length(WARD)) {
-  NAME <- paste("block_",LETTERS[i],sep="")
-  assign(NAME, WARD[i])
-  i <- i+1
+WARD <- list()
+for (block in BLOCKS) {
+  NAME <- gsub("~\\/Box\\/alu\\/spatialData\\/block_(\\w).txt","\\1",block)
+  p <- read.delim(block,header = TRUE)
+  p <- Polygon(p,hole=FALSE)
+  p <- Polygons(list(p),ID = NAME)
+  WARD <- append(WARD,assign(paste("block_",NAME,sep=""),p))
 }
+WARD <- SpatialPolygons(WARD)
+proj4string(WARD) <- CRS("+proj=utm +zone=12 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
+# NEED TO MAKE A DF OF INFO FOR THE WARDS POLYGON
+WARD <- SpatialPolygonsDataFrame(WARD,data.frame())
 
+writeOGR(WARD,dsn=OUTPUT,layer="blocks",driver="ESRI Shapefile")
 
-zip <- readOGR(paste(OUTPUT,"84606_border.shp",sep="/"))
-blocks <-
-plot(zip)
-plot(block_A,add=TRUE)
-lapply(WARD,plot(add=TRUE))
-
-# make ward polygons
-# library(mapview)
-# library(mapedit)
-# library(sf)
-# 
-# MAP <- editMap(mapview())
+# p <- SpatialPolygonsDataFrame(p,data.frame(block=NAME)) use to add data to polygon
 
 # plot stuff - makes map of Utah with addresses
 plotRGB(satTEST,r=1,g=2,b=3,main="Provo Peaks 12th Ward")
