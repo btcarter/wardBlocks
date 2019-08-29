d4971c8ba7554c75a3f0a2d02d6881fdbe0787a9
btcarter <benjamin.t.carter@gmail.com>
2019-08-29 13:54:14 -0600

    it creates a single DF with block information! now to double check my ward block polygons

--- assignBlocks.R
+++ assignBlocks.R
@@ -12,6 +12,7 @@ OUTPUT = file.path("~","Box","alu")               # destination directory for th
 SOURCE = file.path("~","Box","alu","spatialData") # directory with source shapefiles
 A <- file.path(SOURCE,"84606_addresses.shp")      # path to addresses point shapefile
 B <- file.path(SOURCE,"wardBlocks.shp")           # path to ward blocks shapefile
+DIR <- file.path(OUTPUT,"directory.txt")
 
 #### execute ####
 
@@ -26,3 +27,20 @@ D <- A[B,] # crops point data to only ward addresses
 
 # save a table with the data
 write.table(D@data,file = file.path(OUTPUT,"addresses.txt"), append = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+
+# add block assignments to ward directory
+DIR <- read.delim(DIR,header = TRUE, stringsAsFactors = FALSE)  # load directory
+DIR$TEST <- gsub("North","N",DIR$ADDRESS, ignore.case = TRUE) # fix cardinal directions
+DIR$TEST <- gsub("East","E",DIR$TEST, ignore.case = TRUE)
+DIR$TEST <- gsub("West","W",DIR$TEST, ignore.case = TRUE)
+DIR$TEST <- gsub("South","S",DIR$TEST, ignore.case = TRUE)
+DIR$TEST <- gsub("\\s\\#\\d*","",DIR$TEST, ignore.case = TRUE) # remove apartment numbers
+DIR$UTAddPtID <- gsub("(\\d+)\\s(\\w)\\s(.*), Provo, Utah.*","PROVO | \\1 \\2 \\3",DIR$TEST) # parse address column to UTAddPtID format
+DIR$UTAddPtID <- toupper(DIR$UTAddPtID) # convert to uppercase
+DIR$TEST <- NULL
+
+E <- D@data
+E$UTAddPtID <- as.character(E$UTAddPtID)
+
+# get block assignments
+F <- merge(DIR,E, all.x = TRUE)
