c4f4ff9f283489488765ca722d6597f2a400783e
Benjamin Carter <benjamin.t.carter@gmail.com>
2019-08-29 09:17:30 -0600

    finishing script

--- makeLayers.R
+++ makeLayers.R
@@ -33,32 +33,25 @@ writeOGR(utahCounty,dsn=OUTPUT,layer="84606_addresses",driver="ESRI Shapefile")
 AERIAL <- merge(Aerial1,Aerial2)
 writeRaster(AERIAL,paste(OUTPUT,"satImage.tif",sep="/"),format="GTiff",overwrite=TRUE)
 
-# make the ward block polygons
-dfA <- read.delim(paste(OUTPUT,"block_A.txt",sep="/"),header = TRUE)
-dfB <- read.delim(paste(OUTPUT,"block_B.txt",sep="/"),header = TRUE)
 
 # make ward block polygons
 BLOCKS <- c(paste(OUTPUT,"/block_",LETTERS[1:12],".txt",sep=""))
 
 WARD <- list()
-for (block in BLOCKS) {
+i=1
+while (i < length(BLOCKS) + 1) {
+  block = BLOCKS[i]
   NAME <- gsub("~\\/Box\\/alu\\/spatialData\\/block_(\\w).txt","\\1",block)
-  p <- read.delim(block,header = TRUE)
-  p <- Polygon(p,hole=FALSE)
-  p <- Polygons(list(p),ID = NAME)
-  WARD <- append(WARD,assign(paste("block_",NAME,sep=""),p))
+  p <- read.delim(block, header = TRUE)
+  p <- Polygon(p, hole=FALSE)
+  p <- Polygons(list(p), ID = as.numeric(i))
+  WARD <- append(WARD, assign(paste("block_", NAME, sep=""), p))
+  i = i + 1
 }
+  
 WARD <- SpatialPolygons(WARD)
 proj4string(WARD) <- CRS("+proj=utm +zone=12 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
-# NEED TO MAKE A DF OF INFO FOR THE WARDS POLYGON
-WARD <- SpatialPolygonsDataFrame(WARD,data.frame())
+df <- read.delim(paste(OUTPUT,"/blockDF.txt",sep=""),header=TRUE,stringsAsFactors = FALSE)
+WARD <- SpatialPolygonsDataFrame(WARD,data.frame(df))
 
-writeOGR(WARD,dsn=OUTPUT,layer="blocks",driver="ESRI Shapefile")
-
-# p <- SpatialPolygonsDataFrame(p,data.frame(block=NAME)) use to add data to polygon
-
-# plot stuff - makes map of Utah with addresses
-plotRGB(satTEST,r=1,g=2,b=3,main="Provo Peaks 12th Ward")
-plot(HOME,add=TRUE)
-plot(utahCounty,add=TRUE,col="blue")
-
+writeOGR(WARD,dsn=OUTPUT,layer="wardBlocks",driver="ESRI Shapefile",overwrite_layer = TRUE)
